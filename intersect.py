ca582e60861e912511d0da9eb72c4bd8615a7454
mbbutler <m.brady.butler@gmail.com>
2014-05-08 14:37:40 -0400

    Create intersect.py

---
+++ intersect.py
@@ -0,0 +1,24 @@
+import sys
+
+if len(sys.argv) != 3:
+    print "Usage is: python intersect.py [file1] [file2]"
+    exit(0)
+
+
+fname1 = sys.argv[1]
+fname2 = sys.argv[2]
+
+fn1 = open(fname1, 'r')
+fn2 = open(fname2, 'r')
+output = open('insected_file.txt', 'w')
+
+for line1 in fn1:
+    fn2.seek(0,0)
+    for line2 in fn2:
+        if line1 == line2:
+            output.write(line1)
+            break
+
+fn1.close()
+fn2.close()
+output.close()
