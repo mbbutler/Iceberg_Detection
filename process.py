8dfc0c02980af02a2e1bdac02e2cc10ae210d0db
mbbutler <m.brady.butler@gmail.com>
2014-05-14 10:03:37 -0400

    Update process.py

--- process.py
+++ process.py
@@ -9,14 +9,6 @@ import ogr
 import os
 import fnmatch
 from scipy import ndimage
-from scipy.optimize import curve_fit
-
-
-
-def exp_func(x, *p):
-    A, tau = p
-    return A*np.exp(-(x)/(tau))
-
 
 
 ############ MAIN ###########
@@ -60,9 +52,6 @@ for f in files:
         # find_objects returns slices of array that contain each cluster
         iceberg_list = ndimage.find_objects(label_im)
 
-        temp = np.empty(nb_labels)
-        count = 0
-
         day = f[:-8]
         icebergs = []
         for slic in iceberg_list:
@@ -80,34 +69,10 @@ for f in files:
                 line = str(day) + ',' + str(x_cent) + ',' + str(y_cent) + ',' + str(size) + '\n'
                 output.write(line)
                 icebergs.append(new_iceberg)
-                temp[count] = size
-                count += 1
-             
-        hist_temp = np.empty(count)   
-        for i in range(count):
-            hist_temp[i] = temp[i]
-            
-        a_max = np.amax(hist_temp)
-        hist, bin_edges = np.histogram( hist_temp, bins=np.arange(1,a_max+2) )
-        
-        bin_centers = (bin_edges[:-1] + bin_edges[1:])/2
-        
-        p0 = [100., 1.]
-        coeff, var_matrix = curve_fit(exp_func, bin_centers, hist, p0=p0)
-        
-        print 'Fitted Amplitude = ', coeff[0]
-        print 'Fitted decay constant = ', coeff[1]
-        
-        hist_fit = exp_func(bin_centers, *coeff)
-        
-        plt.plot(bin_centers, hist, label='Test data')
-        plt.plot(bin_centers, hist_fit, label='Fitted data')
-        plt.show()
-
-
+     
 ##    db.executemany('INSERT INTO Icebergs VALUES (?,?,?,?)', icebergs)
 ##    con.commit()
-    ds = None
+        ds = None
 ##
 ##con.close()
 output.close()
