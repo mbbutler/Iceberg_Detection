93ce554fe31a1fb0387e84f8d2adf3d4c8bd501c
mbbutler <m.brady.butler@gmail.com>
2014-05-14 10:00:16 -0400

    Create exp_decay.py

---
+++ exp_decay.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+def exp_func(x, *p):
+    A, tau = p
+    return A*np.exp(-(x)/(tau))
+
+
+if len(sys.argv) != 2:
+    print "Usage is: python process.py [file to be processed]"
+    exit()
+    
+fname = sys.argv[1]
+
+hist_temp= np.loadtxt(fname, dtype='uint8', usecols=(3), delimiter=',')
+
+a_max = np.amax(hist_temp)
+hist, bin_edges = np.histogram( hist_temp, bins=np.arange(1,a_max+2) )
+
+bin_centers = (bin_edges[:-1] + bin_edges[1:])/2
+
+p0 = [100., 1.]
+coeff, var_matrix = curve_fit(exp_func, bin_centers, hist, p0=p0)
+
+print 'Fitted Amplitude = ', coeff[0]
+print 'Fitted decay constant = ', coeff[1]
+
+hist_fit = exp_func(bin_centers, *coeff)
+
+plt.plot(bin_centers, hist, label='Test data')
+plt.plot(bin_centers, hist_fit, label='Fitted data')
+plt.show()
